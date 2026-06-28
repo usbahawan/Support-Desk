27d57ddc9f06be6b97883846cf90404120eaac05
usbahawan <usbahawan@users.noreply.github.com>
2026-06-29 01:09:14 +0500

    Set up frontend skeleton

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>SupportDesk Portal</h1>
+      <p>Welcome to the Customer Support Ticket System.</p>
+    </div>
+  );
+}
+
+export default App;
